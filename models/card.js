190c1ffdda14602788bf10c7c240a1ef6b2cc31b
Anatoliy Loskutov <loskutov.tolik@gmail.com>
2022-09-27 22:38:37 +0200

    Work 13. Mesto, Backend

--- models/card.js
+++ models/card.js
@@ -3,13 +3,13 @@ const mongoose = require('mongoose');
 const cardSchema = new mongoose.Schema({
   name: {
     type: String,
-    require: true,
+    required: true,
     minlength: 2,
     maxlength: 30,
   },
   link: {
     type: String,
-    require: true,
+    required: true,
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
