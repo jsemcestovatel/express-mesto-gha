8299504043f642dcd416c68d5d176a0cc5f45e49
Anatoliy Loskutov <loskutov.tolik@gmail.com>
2022-10-16 09:02:51 +0200

    Work 14. Fix. Mesto, Backend with auth

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const routes = require('./routes/index');
-const handlerErrors = require('./middlewares/errors');
+// const handlerErrors = require('./middlewares/errors');
 
 const { PORT = 3000 } = process.env;
+const ERROR_CODE = 500;
 
 const app = express();
 
@@ -24,6 +25,17 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 app.use(routes);
 
 // централизованный обработчик
-app.use(handlerErrors);
+// app.use(handlerErrors);
+
+app.use((err, req, res, next) => {
+  const { statusCode = ERROR_CODE, message } = err;
+  res
+    .status(statusCode)
+    .send({
+      // проверяем статус и выставляем сообщение в зависимости от него
+      message: statusCode === 500 ? 'Ошибка по умолчанию' : message,
+    });
+  next();
+});
 
 app.listen(PORT, () => {});
