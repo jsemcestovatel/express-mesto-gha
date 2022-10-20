4c5ec3b6e7d002e3d1fa41c21bfcc14193a9e4e3
Anatoliy Loskutov <loskutov.tolik@gmail.com>
2022-10-20 21:03:39 +0200

    Work 14. Fix. Mesto, Backend with auth

--- controllers/users.js
+++ controllers/users.js
@@ -56,7 +56,7 @@ module.exports.createUser = (req, res, next) => {
     })
     .catch((err) => {
       if (err.code === 11000) {
-        next(new ConflictError('Пользователь с таким email уже существует.'));
+        next(new ConflictError('Пользователь с таким e-mail уже существует.'));
         return;
       }
       if (err.name === 'ValidationError') {
